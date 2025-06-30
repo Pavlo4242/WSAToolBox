6b3e1e4b20beb4d0beff094c50583bd50895c94d
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-06-30 19:38:27 +0700

    Update SettingsPage.kt

--- WSAToolbox/src/main/kevin/page/pages/SettingsPage.kt
+++ WSAToolbox/src/main/kevin/page/pages/SettingsPage.kt
@@ -3,5 +3,5 @@ package kevin.page.pages
 import kevin.Main
 import kevin.page.Page
 
-class SettingsPage : Page("设置", Main.width/10*5,0,Main.width/10,Main.height/40*3) {
+class SettingsPage : Page("Settings", Main.width/10*5,0,Main.width/10,Main.height/40*3) {
 }
